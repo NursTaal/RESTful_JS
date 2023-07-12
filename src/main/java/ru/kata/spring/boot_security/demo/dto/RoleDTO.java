9412120ffc7715915d462e054bbc6c8f3c967d47
NursTaal <nurs.tnt.123@gmail.com>
2023-07-12 17:28:21 +0600

    Add lombok Annotation

--- src/main/java/ru/kata/spring/boot_security/demo/dto/RoleDTO.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dto/RoleDTO.java
@@ -11,15 +11,4 @@ public class RoleDTO {
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
